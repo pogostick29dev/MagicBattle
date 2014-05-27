4c68246a091ba31dfc34f07ca3cc87180aa8647f
AnUnknownMiner <crash_1703@yahoo.com>
2014-05-27 13:59:36 -0500

    Update List.java

--- src/me/pogostick29dev/magicbattle/commands/List.java
+++ src/me/pogostick29dev/magicbattle/commands/List.java
@@ -1,1 +1,9 @@
 package me.pogostick29dev.magicbattle.commands;
+
+import org.bukkit.entity.Player;
+
+public class List extends MagicCommand {
+  
+  public void onCommand(Player p, String[] args) {
+    //TODO: Make a list of arenas
+  }
